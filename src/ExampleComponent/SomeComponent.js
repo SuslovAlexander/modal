1dbf9a13fa9846b7c81238166ce5e4cd11b3e143
SuslovAlexander <suslov.itrum@gmail.com>
2023-04-26 10:46:56 +0300

    add modal logic

---
+++ src/ExampleComponent/SomeComponent.js
@@ -0,0 +1,9 @@
+import styles from './SomeComponent.module.css';
+
+const SomeComponent = () => {
+    return (
+        <div className={styles.test}>Some Component</div>
+    )
+}
+
+export default SomeComponent;
---
+++ src/TestComponent.js
@@ -0,0 +1,16 @@
+const TestComponent = () => {
+    return (
+        <div style={{
+            width: "500px",
+            height: "500px",
+            background: "lightblue",
+            borderRadius: "24px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "#fff",
+            fontSize: '42px',
+        }}>!!!</div>
+    )
+}
+export default TestComponent;
